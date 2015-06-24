e8c217a45e65840acfee7ba994fbedc947d716d0
Nathan Stier <nathan.stier@gmail.com>
2015-06-24 01:26:00 -0500

    fix jasmine file loading

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 var Jasmine = require('jasmine')
 var jasmine = new Jasmine()
 
-jasmine.loadConfigFile('spec/support/jasmine.json')
+console.log(__dirname)
+console.log(process.cwd())
+
+jasmine.loadConfig({
+    spec_dir: 'node_modules/ywca-chapter01/spec/',
+    spec_files: [
+        'spec.js'
+    ],
+    helpers: [
+        'environment.js',
+        '../../../chapter01/index.js'
+    ]
+});
 
 jasmine.execute()
